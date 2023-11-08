8bff6f3eb59c12838b387111ee936f629e0dc570
Mohamed Gueye <mohamed.gueye.external@airbus.com>
2023-11-08 15:40:52 +0100

    Remove weather dependency of location and add behaviour subject instead

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {WeatherService} from "./weather.service";
+import { BehaviorSubject } from 'rxjs';
 
 export const LOCATIONS : string = "locations";
 
@@ -8,18 +8,23 @@ export class LocationService {
 
   locations : string[] = [];
 
-  constructor(private weatherService : WeatherService) {
+  //expose three observables to emits zipcodes on changes
+  locations$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+  addLocation$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  removeLocation$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  constructor() {
     let locString = localStorage.getItem(LOCATIONS);
-    if (locString)
+    if (locString) {
       this.locations = JSON.parse(locString);
-    for (let loc of this.locations)
-      this.weatherService.addCurrentConditions(loc);
+      this.locations$.next(this.locations);
+    }
   }
 
   addLocation(zipcode : string) {
     this.locations.push(zipcode);
     localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-    this.weatherService.addCurrentConditions(zipcode);
+    this.addLocation$.next(zipcode);
   }
 
   removeLocation(zipcode : string) {
@@ -27,7 +32,7 @@ export class LocationService {
     if (index !== -1){
       this.locations.splice(index, 1);
       localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-      this.weatherService.removeCurrentConditions(zipcode);
+      this.removeLocation$.next(zipcode);
     }
   }
 }
