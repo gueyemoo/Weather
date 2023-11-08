9f261c2d64d1d3de978eacc7e69a179a105bf4d4
Mohamed Gueye <mohamed.gueye.external@airbus.com>
2023-11-08 15:42:17 +0100

    Update current condition to be based on behaviour subject emitted

--- src/app/current-conditions/current-conditions.component.ts
+++ src/app/current-conditions/current-conditions.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Signal} from '@angular/core';
+import { Component, inject, OnInit, Signal } from '@angular/core';
 import {WeatherService} from "../weather.service";
 import {LocationService} from "../location.service";
 import {Router} from "@angular/router";
@@ -9,14 +9,43 @@ import {ConditionsAndZip} from '../conditions-and-zip.type';
   templateUrl: './current-conditions.component.html',
   styleUrls: ['./current-conditions.component.css']
 })
-export class CurrentConditionsComponent {
+export class CurrentConditionsComponent implements OnInit {
 
   private weatherService = inject(WeatherService);
   private router = inject(Router);
   protected locationService = inject(LocationService);
   protected currentConditionsByZip: Signal<ConditionsAndZip[]> = this.weatherService.getCurrentConditions();
 
-  showForecast(zipcode : string){
+  ngOnInit(): void {
+    this.addCurrentCondition();
+    this.removeCurrentCondition();
+  }
+
+  /**
+  * @description Subscribes to subject and remove the current conditions to the weatherService 
+  * when a zipcode is received.
+  */
+  private removeCurrentCondition(): void {
+    this.locationService.removeLocation$.subscribe((zipcode) => {
+      if (zipcode) {
+        this.weatherService.removeCurrentConditions(zipcode);
+      }
+    });
+  }
+
+  /**
+  * @description Subscribes to subject and add the current conditions to the weatherService 
+  * when a zipcode is received.
+  */
+  private addCurrentCondition(): void {
+    this.locationService.addLocation$.subscribe((zipcode) => {
+      if (zipcode) {
+        this.weatherService.addCurrentConditions(zipcode);
+      }
+    });
+  }
+
+  public showForecast(zipcode: string): void {
     this.router.navigate(['/forecast', zipcode])
   }
 }
