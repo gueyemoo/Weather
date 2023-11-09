1a93681511c12719c3f787aedbafde9ef0f3ebcc
Mohamed Gueye <mohamed.gueye.external@airbus.com>
2023-11-09 17:03:54 +0100

    Update remove function of current-conditon component

--- src/app/components/main-page/current-conditions/current-conditions.component.ts
+++ src/app/components/main-page/current-conditions/current-conditions.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit, Signal } from '@angular/core';
 import { WeatherService } from "../../../common/services/weather.service";
-import { LocationService } from "../../../common/services/location.service";
+import { LOCATIONS, LocationService } from "../../../common/services/location.service";
 import { Router } from "@angular/router";
 import { ConditionsAndZip } from '../../../common/types/conditions-and-zip.type';
 
@@ -11,26 +11,32 @@ import { ConditionsAndZip } from '../../../common/types/conditions-and-zip.type'
 })
 export class CurrentConditionsComponent implements OnInit {
 
+  private locations: string[] = [];
   private weatherService = inject(WeatherService);
   private router = inject(Router);
   protected locationService = inject(LocationService);
   protected currentConditionsByZip: Signal<ConditionsAndZip[]> = this.weatherService.getCurrentConditions();
 
+
   ngOnInit(): void {
+    //temporary use local storage for init here for test, to clean up
+    console.log('currentConditionsByZip(): ', this.currentConditionsByZip());
+    let locString = localStorage.getItem(LOCATIONS);
+    if (locString) {
+      this.locations = JSON.parse(locString);
+    }
+    this.weatherService.loadCurrentConditions(this.locations);
     this.addCurrentCondition();
-    this.removeCurrentCondition();
   }
 
   /**
   * @description Subscribes to subject and remove the current conditions to the weatherService 
   * when a zipcode is received.
   */
-  private removeCurrentCondition(): void {
-    this.locationService.removeLocation$.subscribe((zipcode) => {
-      if (zipcode) {
-        this.weatherService.removeCurrentConditions(zipcode);
-      }
-    });
+  public removeCurrentCondition(tabIndex: number): void {
+    const filteredZipcode = this.currentConditionsByZip()[tabIndex].zip;
+    this.locationService.removeLocation(filteredZipcode);
+    this.weatherService.removeCurrentConditions(filteredZipcode);
   }
 
   /**
