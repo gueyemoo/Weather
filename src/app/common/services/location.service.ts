a57c836fb149390ab6b280baf7d5e8cc9f8399f0
Mohamed Gueye <mohamed.gueye.external@airbus.com>
2023-11-09 17:01:32 +0100

    Change service and use localStorage in current condition for test

--- src/app/common/services/location.service.ts
+++ src/app/common/services/location.service.ts
@@ -11,17 +11,19 @@ export class LocationService {
   //expose three observables to emits zipcodes on changes
   locations$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
   addLocation$: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  removeLocation$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor() {
-    let locString = localStorage.getItem(LOCATIONS);
-    if (locString) {
-      this.locations = JSON.parse(locString);
-      this.locations$.next(this.locations);
-    }
+    // let locString = localStorage.getItem(LOCATIONS);
+    // if (locString) {
+    //   this.locations = JSON.parse(locString);
+    //   this.locations$.next(this.locations);
+    // }
   }
 
   addLocation(zipcode : string) {
+    if (this.locations.indexOf(zipcode) !== -1) {
+      return alert('Location exist');
+    }
     this.locations.push(zipcode);
     localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
     this.addLocation$.next(zipcode);
@@ -32,7 +34,7 @@ export class LocationService {
     if (index !== -1){
       this.locations.splice(index, 1);
       localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-      this.removeLocation$.next(zipcode);
+      this.locations$.next(this.locations);
     }
   }
 }
