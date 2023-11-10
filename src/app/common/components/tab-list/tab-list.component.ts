8f66a3f5ad95768acf2499cda7fb30f83222180b
Mohamed Gueye <mohamed.gueye.external@airbus.com>
2023-11-10 14:46:36 +0100

    add possibility to put a template for the title as well on tab-list

--- src/app/common/components/tab-list/tab-list.component.ts
+++ src/app/common/components/tab-list/tab-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChildren, EventEmitter, Input, Output, QueryList, TemplateRef } from '@angular/core';
+import { Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList, TemplateRef } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -6,34 +6,38 @@ import { TabComponent } from '../tab/tab.component';
   templateUrl: './tab-list.component.html',
   styleUrls: ['./tab-list.component.css']
 })
-export class TabListComponent implements AfterContentInit {
+export class TabListComponent {
 
-  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent<any>>;
 
   @Output()
-  removeTab = new EventEmitter<number>();
+  removeTab: EventEmitter<number> = new EventEmitter<number>();
 
-  ngAfterContentInit() { //TODO: fix using index instead of boolean
-    // const activeTabs = this.tabs.filter(tab => tab.active);
-
-    // if (activeTabs.length === 0) {
-    //   this.selectTab(this.tabs.first);
-    // }
+  /**
+   * @description Check if the title is a string
+   * @param title a string or template to be displayed as title of tab
+   */
+  public isString(title: string | TemplateRef<any>): boolean {
+    return typeof title === "string";
   }
 
-  selectTab(tab: TabComponent) {
-    if (tab) {
-      this.tabs.forEach(tab => (tab.active = false));
-      tab.active = true;
-    }
+  /**
+   * @description Allow to change active tab on click
+   * @param clickedTabIndex index of selected tab by user
+   */
+  public selectTab(clickedTabIndex: number): void {
+    this.tabs.toArray().forEach((tab, index) => {
+      tab.active = index === clickedTabIndex;
+    });
   }
 
-  closeTab(event: PointerEvent, clickedTabIndex: number) {
+  /**
+   * @description allow to close the tab on click
+   * @param clickedTabIndex index of selected tab by user
+   * @param event pointer event click
+   */
+  public closeTab(clickedTabIndex: number, event: PointerEvent) {
     event.stopPropagation(); //prevent to propagate click to activate tab
-    if (clickedTabIndex > 0) {
-      const newActiveTab = this.tabs[clickedTabIndex - 1];
-      this.selectTab(newActiveTab)
-    }
     this.removeTab.emit(clickedTabIndex);
   }
 
